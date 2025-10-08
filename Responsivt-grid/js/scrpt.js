4a29daf5b00c67752d48fb8ab578c59d42f17b63
klpaulse <lovise_paulsen@hotmail.com>
2025-10-08 14:23:05 +0200

    mappe
    lagt il mer

---
+++ Responsivt-grid/js/scrpt.js
@@ -0,0 +1,12 @@
+const ham = document.getElementById(`ham`)
+const closeM = document.getElementById(`close`)
+const menu = document.getElementById(`menu`)
+
+
+ham.addEventListener(`click`, () => {
+    menu.classList.toggle(`hidden`)
+})
+
+closeM.addEventListener(`click`, () => {
+    menu.classList.toggle(`hidden`)
+})
